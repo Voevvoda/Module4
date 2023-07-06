6d731c990efe6d79269ceae5b687e0aa3efe88ed
Voevvodda <mminitanos@gmail.com>
2023-07-06 22:15:32 +0300

    ewfcerce

---
+++ Les_1/1.py
@@ -0,0 +1,26 @@
+"""def strcounter(s):
+    for sym in set(s):
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+
+strcounter('abckfaa')"""
+        
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+strcounter('abcdeaa')            
+
+
+#ewffweffwefe
+#dfwef
+#
+#
+#
+#wqswqwqe
